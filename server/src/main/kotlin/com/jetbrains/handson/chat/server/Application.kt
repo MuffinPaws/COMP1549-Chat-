573b06a383874a7bcc09827fff33ce02693354dc
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-26 14:45:52 +0000

    sever comments

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -11,14 +11,19 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused")
 fun Application.module() {
+    //use KTOR websockets
     install(WebSockets)
+    //for each client connection create a KTOR thread
     routing {
         // list of all clients
         val connections = Collections.synchronizedSet<Connection?>(LinkedHashSet())
         //server listens on path /chat
         webSocket("/chat") {
+            //when user connects log
             println("Adding user!")
+            //create a connection instance using the Connection model
             val thisConnection = Connection(this)
+            //add connection to list of all clients
             connections += thisConnection
             try {
                 send("You are connected! There are ${connections.count()} users here.")
