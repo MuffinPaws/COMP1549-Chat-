8a272c3ce1d68faee0008204dd87f33c3be50b42
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-26 16:26:39 +0000

    added to class OperatingParameters CLI parameters

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -1,6 +1,9 @@
 package com.jetbrains.handson.chat.client
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.prompt
+import com.github.ajalt.clikt.parameters.types.int
 import io.ktor.client.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.*
@@ -13,11 +16,11 @@ fun main() {
     }
     runBlocking {
         client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-            while(true) {
+            while (true) {
                 val othersMessage = incoming.receive() as? Frame.Text ?: continue
                 println(othersMessage.readText())
                 val myMessage = readLine()
-                if(myMessage != null) {
+                if (myMessage != null) {
                     send(myMessage)
                 }
             }
@@ -51,7 +54,12 @@ suspend fun DefaultClientWebSocketSession.outputMessages() {
     }
 }
 
-class OperatingParameters: CliktCommand(){
+class OperatingParameters : CliktCommand() {
+    val name: String by option(help = "Your display name").prompt("What is your display name")
+    val serverIP: String by option(help = "The server's IP address ").prompt("What is the server's IP address")
+    val serverPort: Int by option(help="The server's port").int().prompt("What is the port at the server")
+    val clientIP: String by option(help = "Which IP address to use").prompt("What is the IP address you want to use")
+    val clientPort: Int by option(help="Which port to use").int().prompt("What is the port you want to use")
 
     override fun run() {
         TODO("Not yet implemented")
