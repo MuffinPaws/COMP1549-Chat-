2bbe579092025ed01f223d4141978915c35a3c69
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-26 15:04:21 +0000

    Connection model comments finished

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
@@ -3,9 +3,13 @@ package com.jetbrains.handson.chat.server
 import io.ktor.http.cio.websocket.*
 import java.util.concurrent.atomic.AtomicInteger
 
+//Connection model
 class Connection(val session: DefaultWebSocketSession) {
+    //singleton instance shared by all connections
     companion object {
+        //sequential unique ID generator TODO change to client finger print
         var lastId = AtomicInteger(0)
     }
+    //client DATA
     val name = "user${lastId.getAndIncrement()}"
 }
