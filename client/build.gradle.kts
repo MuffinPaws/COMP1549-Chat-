44eb46b551ed1fc62bb6780e479fd97e11b7120f
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-27 16:36:41 +0100

    Updated KTOR

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -7,12 +7,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        name = "ktor-eap"
+    }
 }
 
 val ktor_version: String by project
 
 dependencies {
-    implementation("io.ktor:ktor-client-websockets:$ktor_version")
-    implementation("io.ktor:ktor-client-cio:$ktor_version")
     implementation("com.github.ajalt.clikt:clikt:3.4.0")
+    implementation("io.ktor:ktor-client-websockets-jvm:2.0.0-eap-256")
+    implementation("io.ktor:ktor-client-cio-jvm:2.0.0-eap-256")
 }
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,13 +7,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.jetbrains.space/public/p/ktor/eap")
+        name = "ktor-eap"
+    }
 }
 
 val ktor_version: String by project
 val logback_version: String by project
 
 dependencies {
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-netty-jvm:2.0.0-eap-256")
+    implementation("io.ktor:ktor-server-websockets-jvm:2.0.0-eap-256")
 }
