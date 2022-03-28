6baeec8b925f1f9f6f9ecf529307b6cff6c7d279
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-28 23:56:53 +0100

    code cleanup Message.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
@@ -3,52 +3,46 @@ package com.jetbrains.handson.chat.client
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Message(val data:String){
+data class Message(val data: String) {
     //List of all possible message type the app can receive
-    enum class AplicationDataType{
-        TEXT,
-        FILE,
-        PING,;
+    enum class AplicationDataType {
+        TEXT, FILE, PING, ;
 
-        companion object{
+        companion object {
             fun findDataType(name: String): AplicationDataType {
-                for (enum in values()){
+                for (enum in values()) {
                     if (enum.name.equals(name, true)) return enum
                 }
                 return PING
             }
         }
-
-        val json = """"""
     }
-
-
 }
 
 //TODO fix comment bellow
 // abstract class for providing a framework for storing T data
-abstract class MessageBox<T>(t: T){
+abstract class MessageBox<T>(t: T) {
     val data = t
-    open fun serialise(){
+    open fun serialise() {
         TODO("The serialise function has not been created😒")
     }
 }
 
-class TextMessageBox(data: String):MessageBox<String>(data){
+class TextMessageBox(data: String) : MessageBox<String>(data) {
     override fun serialise() {
         TODO("Not yet implemented")
     }
 
 }
 
-class FileMessageBox(data: String):MessageBox<String>(data){
+class FileMessageBox(data: String) : MessageBox<String>(data) {
     override fun serialise() {
         TODO("Not yet implemented")
     }
 
 }
 
-class PingMessageBox(ID: Int):MessageBox<Int>(ID){
+class PingMessageBox(ID: Int) : MessageBox<Int>(ID) {
     override fun serialise() {
         TODO("Not yet implemented")
     }
