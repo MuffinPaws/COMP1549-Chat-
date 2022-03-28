017a2977e9f4fb1169c3f98ab9da2577f87f15f7
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-28 11:53:04 +0100

    updated comments in Message.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
@@ -10,8 +10,6 @@ data class Message(val data:String){
 //TODO fix comment bellow
 // abstract class for providing a framework for storing T data
 open class messageBox<T>(data: T){
-    // TODO Need to check with bellissimo and samuele about how they are handing incoming frames
-    // Is it only AS Text or Any?
     open fun serialise(){
         TODO("The serialise function has not been created😒")
     }
