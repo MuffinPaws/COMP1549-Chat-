2cf908e6dd28523d11da7ed76afef8b51e39b5dd
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-28 10:03:18 +0100

    added messageBox

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
@@ -1,3 +1,13 @@
 package com.jetbrains.handson.chat.client
 
-data class Message(val data:String)
+data class Message(val data:String){
+    enum class dataType{
+
+    }
+}
+
+open class messageBox<T>(data: T){
+    open fun serialise(){
+        TODO("The serialise function has not been created😒")
+    }
+}
