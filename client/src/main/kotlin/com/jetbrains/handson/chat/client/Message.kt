eb7f01806d799f3d835f5562e1c3cde7e5098a32
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-28 10:11:25 +0100

    Question about incoming frame handling

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
@@ -1,12 +1,17 @@
 package com.jetbrains.handson.chat.client
 
 data class Message(val data:String){
-    enum class dataType{
+    //List of all possible message type the app can receive
+    enum class AplicationDataType{
 
     }
 }
 
+//TODO fix comment bellow
+// abstract class for providing a framework for storing T data
 open class messageBox<T>(data: T){
+    // TODO Need to check with bellissimo and samuele about how they are handing incoming frames
+    // Is it only AS Text or Any?
     open fun serialise(){
         TODO("The serialise function has not been created😒")
     }
