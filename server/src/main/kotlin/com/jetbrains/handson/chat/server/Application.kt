8c225d605203d4a3409141a902904aa220d6f51a
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-30 12:24:49 +0100

    init data class clientData.kt in server

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Application.kt
@@ -5,7 +5,6 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.routing.*
 import io.ktor.websocket.*
 import java.util.*
-import kotlin.collections.LinkedHashSet
 
 // args are collected from HODEL and passed to Netty server
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
---
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/clientData.kt
@@ -0,0 +1,3 @@
+package com.jetbrains.handson.chat.server
+
+data class clientData(val name: String, val ID: String, val IP: String, val port: Int)
