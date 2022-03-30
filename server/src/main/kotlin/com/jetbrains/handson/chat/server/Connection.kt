710654d317e524b0c71f72c1fdd6569d4cec5c87
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-30 15:28:22 +0100

    init code clean

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
@@ -3,10 +3,7 @@ package com.jetbrains.handson.chat.server
 import io.ktor.http.cio.websocket.*
 
 //Connection model
-//TODO add val ID drop companion
 class Connection(val session: DefaultWebSocketSession, val clientData: clientData) {
-
     // by default no connection/member is coordinator
     var isCoord = false
-
 }
