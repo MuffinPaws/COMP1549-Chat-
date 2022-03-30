357b10213cc6054e77c102b3860a7e498b55ba58
samacciu23 <samuele.damar@outlook.it>
2022-03-30 09:24:12 +0100

    added test directories and files

---
+++ server/src/test/kotlin/com/jetbrains/handson/chat/server/test/ApplicationKtTest.kt
@@ -0,0 +1,24 @@
+package com.jetbrains.handson.chat.server.test
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ApplicationKtTest {
+
+    @Test
+    fun main() {
+    }
+
+    @Test
+    fun module() {
+    }
+
+    @Test
+    fun setNewCoord() {
+    }
+
+    @Test
+    fun getExisistingMembers() {
+    }
+}
