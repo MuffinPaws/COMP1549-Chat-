1dc63a89a08f3e04ab3190396fcff7243f047667
samacciu23 <samuele.damar@outlook.it>
2022-03-30 15:03:11 +0100

    added test implementations in build.gradle.kts file and updated ApplicationKtTest.kt file

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -18,6 +18,9 @@ dependencies {
     implementation("io.ktor:ktor-websockets:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.6.10")
 }
 /*
  ktor-server-netty adds Ktor together with the Netty engine, allowing us to use server functionality without having to rely on an external application container.
--- server/src/test/kotlin/com/jetbrains/handson/chat/server/test/ApplicationKtTest.kt
+++ server/src/test/kotlin/com/jetbrains/handson/chat/server/test/ApplicationKtTest.kt
@@ -1,18 +1,32 @@
 package com.jetbrains.handson.chat.server.test
 
+import com.jetbrains.handson.chat.server.*
 import org.junit.jupiter.api.Test
+import io.ktor.application.*
+import io.ktor.http.cio.websocket.*
+import io.ktor.server.testing.*
 
-import org.junit.jupiter.api.Assertions.*
 
 internal class ApplicationKtTest {
 
     @Test
-    fun main() {
+    fun testConversation() {
+        withTestApplication(Application::module) {
+            handleWebSocketConversation("/echo") { incoming, outgoing ->
+                val greetingText = (incoming.receive() as Frame.Text).readText()
+                kotlin.test.assertEquals("Please enter your name", greetingText)
+
+                outgoing.send(Frame.Text("JetBrains"))
+                val responseText = (incoming.receive() as Frame.Text).readText()
+                kotlin.test.assertEquals("Hi, JetBrains!", responseText)
+
+                outgoing.send(Frame.Text("bye"))
+                val closeReason = (incoming.receive() as Frame.Close).readReason()?.message
+                kotlin.test.assertEquals("Client said BYE", closeReason)
+            }
+        }
     }
 
-    @Test
-    fun module() {
-    }
 
     @Test
     fun setNewCoord() {
