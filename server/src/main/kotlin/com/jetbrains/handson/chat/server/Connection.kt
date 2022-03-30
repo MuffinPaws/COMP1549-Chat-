98fe03549d5bb5268e702f3648bcf24db6b32d10
MuffinPaws <77940381+MuffinPaws@users.noreply.github.com>
2022-03-30 15:27:02 +0100

    init code clean

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/Connection.kt
@@ -6,13 +6,7 @@ import io.ktor.http.cio.websocket.*
 //TODO add val ID drop companion
 class Connection(val session: DefaultWebSocketSession, val clientData: clientData) {
 
-    // by default no connection/member is coordinator (set to zero)
+    // by default no connection/member is coordinator
     var isCoord = false
 
 }
-
-/*
- Note that we are using AtomicInteger as a thread-safe data structure for the counter.
- ensures that two users will never receive the same ID for their username –
- even when their two Connection objects are created simultaneously on separate threads.
- */
