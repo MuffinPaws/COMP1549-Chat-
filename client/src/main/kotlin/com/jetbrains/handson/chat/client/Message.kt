87687b10896847e463d03a50b1d7d2d5147e4802
MuffinPaws😽 <77940381+MuffinPaws@users.noreply.github.com>
2022-03-31 07:40:25 +0100

    fixed incoming client list parsing in ConfigMessageBox in Message.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message.kt
@@ -83,7 +83,7 @@ class PingMessageBox(ID: Int) : MessageBox<Int>(ID) {
 
 class ConfigMessageBox(dataB64: String) : MessageBox<String>(dataB64) {
     //TODO add detect if coord now and show user "You are the coordinator my friend!"
-    private val dataParsed = Base64.getUrlDecoder().decode(data).toString()
+    private val dataParsed = String(Base64.getUrlDecoder().decode(dataB64))
     fun updateMembers(){
         val newClientLis = Json.decodeFromString<List<clientData>>(dataParsed)
         println(newClientLis.joinToString())//TODO remove
