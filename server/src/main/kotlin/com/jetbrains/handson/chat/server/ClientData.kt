17be8edd2c648228a92eca5648d0df141df97e1a
MuffinPaws😽 <77940381+MuffinPaws@users.noreply.github.com>
2022-03-31 22:03:16 +0100

    add comments to ClientData.kt

--- server/src/main/kotlin/com/jetbrains/handson/chat/server/ClientData.kt
+++ server/src/main/kotlin/com/jetbrains/handson/chat/server/ClientData.kt
@@ -3,7 +3,8 @@ package com.jetbrains.handson.chat.server
 import kotlinx.serialization.EncodeDefault
 import kotlinx.serialization.Serializable
 
+// ClientData is a model for deserialization of message headers
 @Serializable
-data class clientData(
+data class ClientData(
     val name: String, val ID: String, val IP: String, val port: Int, @EncodeDefault var isCoord: Boolean = false
 )
