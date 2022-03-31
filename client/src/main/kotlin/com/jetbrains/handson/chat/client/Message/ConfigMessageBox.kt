cda639ffebc8e882dfbb11de168d6caad73bd913
MuffinPaws😽 <77940381+MuffinPaws@users.noreply.github.com>
2022-03-31 22:28:57 +0100

    finished comment ConfigMessageBox.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message/ConfigMessageBox.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message/ConfigMessageBox.kt
@@ -1,4 +1,4 @@
-package com.jetbrains.handson.chat.client
+package com.jetbrains.handson.chat.client.Message
 
 import com.jetbrains.handson.chat.client.allClientsData.allClients
 import com.jetbrains.handson.chat.client.allClientsData.clientData
@@ -6,6 +6,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import java.util.*
 
+// Functions to process CONFIG messages
 class ConfigMessageBox(dataB64: String) : MessageBox<String>(dataB64) {
     private val dataParsed = String(Base64.getUrlDecoder().decode(dataB64))
     fun updateMembers() {
