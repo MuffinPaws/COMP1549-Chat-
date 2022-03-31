f34d77e573d0410a1b2ba4d90573d9610abdc945
MuffinPaws😽 <77940381+MuffinPaws@users.noreply.github.com>
2022-03-31 22:29:32 +0100

    finished comment and refactor for ApplicationDataType.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/Message/ApplicationDataType.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/Message/ApplicationDataType.kt
@@ -1,15 +1,6 @@
-package com.jetbrains.handson.chat.client
+package com.jetbrains.handson.chat.client.Message
 
 //List of all possible message type the app can receive
 enum class ApplicationDataType {
     TEXT, FILE, CONFIG;
-
-    companion object {
-        fun findDataType(name: String): ApplicationDataType {
-            for (enum in values()) {
-                if (enum.name.equals(name, true)) return enum
-            }
-            return TEXT
-        }
-    }
 }
