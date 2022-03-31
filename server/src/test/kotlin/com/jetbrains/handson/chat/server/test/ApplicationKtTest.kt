e5b56c994d80574984cfbb4cc9ef9e031b09e9fc
samacciu23 <samuele.damar@outlook.it>
2022-03-31 23:19:39 +0100

    fixed red warning in ApplicationKtTest.kt

--- server/src/test/kotlin/com/jetbrains/handson/chat/server/test/ApplicationKtTest.kt
+++ server/src/test/kotlin/com/jetbrains/handson/chat/server/test/ApplicationKtTest.kt
@@ -10,7 +10,7 @@ internal class ApplicationKtTest {
 
     @Test
     fun testApplicationThatShouldPass() {
-        withTestApplication({ module(testing = true) }) {
+        withTestApplication({ module() }) {
             handleRequest(HttpMethod.Get, "/chat").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello, world!", response.content)
@@ -20,7 +20,7 @@ internal class ApplicationKtTest {
 
     @Test
     fun testApplicationThatShouldFail() {
-        withTestApplication({ module(testing = true) }) {
+        withTestApplication({ module() }) {
             handleRequest(HttpMethod.Get, "/chat").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("Hello world!", response.content)
